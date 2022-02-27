7735e0958371579225015883dcc4d7e5ca7fae1c
Shouryam Kumar <shouryam1508@gmail.com>
2022-02-27 09:16:21 +0530

    fifth bug fix

--- hardhar-tutorial/scripts/deploy.js
+++ hardhar-tutorial/scripts/deploy.js
@@ -1,34 +1,30 @@
 const { ethers } = require("hardhat");
 
-async function main(){
-    /*
-        A contractFactory  in ethers.js is an abstraction used to deploy new smart contracts,
-        so whitelistcontract here is a factory for instance of our Whitelist contract.
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-    */
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
+  
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-    const whitelistContract = await ethers.getContractFactory("Whitelist");
-
-    //here we deploy the contract
-
-    const deployedWhitelistContract = await whitelistContract.deploy(10);
-    //10 is the maximum no. of whitelisted addresses allowed!
-
-    //wait for it to finish deploying
-
-    await deployedWhitelistContract.deployed();
-
-    //print the address of t he deployed contract
-    console.log("WhielistContrac ADDRESS: ", deployedWhitelistContract.address);
-
-
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address:",
+    deployedWhitelistContract.address
+  );
 }
 
-//Call the main function and catch errors if any
-
+// Call the main function and catch if there is any error
 main()
-    .then(()=> process.exit(0))
-    .catch((error)=> {
-        console.log(error);
-        process.exit(1);
-    })
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
